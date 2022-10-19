3dc64258356f9e3996308429da2f75b6171fde09
haruki111 <yuji515091@gmail.com>
2022-10-19 20:39:06 +0900

    feat: account

---
+++ accounts/models.py
@@ -0,0 +1,49 @@
+import uuid
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.utils import timezone
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('メールアドレスは必須です')
+
+        email = self.normalize_email(email)
+        email = email.lower()
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        user = self.create_user(email, password, **extra_fields)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(verbose_name='メールアドレス',
+                              max_length=255, unique=True)
+    name = models.CharField(verbose_name='名前', max_length=20)
+    icon = models.ImageField(
+        upload_to='accounts', verbose_name='プロフィール画像', blank=True, null=True)
+    profile = models.TextField(
+        verbose_name='自己紹介', blank=True, null=True, max_length=100)
+    created_at = models.DateTimeField(verbose_name='入会日', default=timezone.now)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
